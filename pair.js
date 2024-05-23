b4732b41e74e680d4b265b3c58de15b5b9e1ce08
Freddy Phoenix Mills <118795606+anonphoenix007@users.noreply.github.com>
2024-05-23 08:08:10 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -53,9 +53,9 @@ router.get('/', async (req, res) => {
                 await delay(5000);
                 let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                 await delay(800);
-               //let b64data = Buffer.from(data).toString('base64');
-               //let session = await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: 'MAKINO-MD-V2;;;' + b64data });
-                 let session = await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: data });
+               let b64data = Buffer.from(data).toString('base64');
+               let session = await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: 'MAKINO-MD-V2;;;' + b64data });
+                 //let session = await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: data });
 
                  let SIGMA_MD_TEXT = `
     *_Pair Successful🥵._*
